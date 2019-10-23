1e9d3e5509ba46cf86a8694a4b83d90644c55a51
denlog1977 <denlog1977@gmail.com>
2019-10-23 20:37:13 +0300

    try

--- app/src/main/java/com/example/a13healthmonitoringsystem/MainActivity.java
+++ app/src/main/java/com/example/a13healthmonitoringsystem/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,15 +19,29 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText editTextFIO = (EditText) findViewById(R.id.editTextFIO);
         final EditText editTextAge = (EditText) findViewById(R.id.editTextAge);
-        final EditText editText5 = (EditText) findViewById(R.id.editText5);
 
         Button buttonSave = (Button) findViewById(R.id.buttonSave);
         buttonSave.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
-                    String fio = editText5.getText();
-                    //Integer.parseInt(editTextAge.getText().toString())
-                    Patient patient = new Patient(editTextFIO.getText(), 12);
+                    int age = 0;
+                    boolean success = false;
+                    try {
+                        age = Integer.parseInt(editTextAge.getText().toString());
+                        if (age > 100 || age < 1){
+                            Toast.makeText(MainActivity.this, "Введите число от 1 до 100", Toast.LENGTH_LONG).show();
+                        } else {
+                            success = true;
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "Введите число от 1 до 100", Toast.LENGTH_LONG).show();
+                    }
+                    if (success) {
+                        Patient patient = new Patient(editTextFIO.getText().toString(), age);
+                        Toast.makeText(MainActivity.this, patient.toString(), Toast.LENGTH_LONG).show();
+                    } else {
+                        editTextAge.setText("Введите число от 1 до 100");
+                    }
                 }
             }
         );
